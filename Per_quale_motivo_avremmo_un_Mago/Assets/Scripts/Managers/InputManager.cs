111823bd3b9d78a3eb91a1cbd4424db2f0d62563
Francioo87NABA <francesco_palma@studenti.naba.it>
2020-06-14 19:23:11 +0200

    VRplayer + InputManager
    reimportato steam vr perchè non si vedevano le mani in play, ora funziona tutto, aggiunto l'inputManager con riferimento alle mani del player

---
+++ Per_quale_motivo_avremmo_un_Mago/Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Singleton;
+
+    [Header("SteamVR References")]
+    public Hand leftHand;
+    public Hand rightHand;
+
+    private void OnEnable()
+    {
+        Singleton = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
