20635512bec86a5222213ebfdb5c11e4bb7f3ca7
Francioo87NABA <francesco_palma@studenti.naba.it>
2020-07-20 23:24:51 +0200

    Itween
    tornado collegato

--- Per_quale_motivo_avremmo_un_Mago/Assets/Scripts/Managers/SpellManager.cs
+++ Per_quale_motivo_avremmo_un_Mago/Assets/Scripts/Managers/SpellManager.cs
@@ -24,6 +24,8 @@ public class SpellManager : MonoBehaviour
     public bool metamorfosi;
     public GameObject proiettile;
     public Transform wandRay;
+    public GameObject tornado;
+    public Transform tornadoInstantiation;
 
     //public Transform spellManager;
 
@@ -72,6 +74,13 @@ public class SpellManager : MonoBehaviour
         {
             StartCoroutine(Proiettili());
         }
+
+        if (spellToCast.spellName == "Tornado")
+        {
+            StartCoroutine(Tornado());
+        }
+
+
     }
 
     IEnumerator Metamorfosi()
@@ -88,5 +97,12 @@ public class SpellManager : MonoBehaviour
             Instantiate(proiettile, wandRay.position, wandRay.rotation);
             yield return new WaitForSecondsRealtime(0.5f);
         }
+    }
+
+    IEnumerator Tornado()
+    {
+        Instantiate(tornado, tornadoInstantiation.position, tornadoInstantiation.rotation);
+        yield return new WaitForSecondsRealtime(5f);
+        Destroy(tornado);
     }
 }
---
+++ Per_quale_motivo_avremmo_un_Mago/Assets/Scripts/Spells/Maggie/Tornado.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tornado : MonoBehaviour
+{
+    public iTween.EaseType movementEasyType = iTween.EaseType.easeInOutSine;
+
+    public float movementRadius = 30f;
+    public float movementSpeed = 0.5f;
+
+    private Vector3 originalPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPosition = transform.position;
+        StartCoroutine(Movement());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Movement()
+    {
+        var newPos = new Vector3(originalPosition.x + Random.Range(-movementRadius, movementRadius), originalPosition.y, originalPosition.z + Random.Range(-movementRadius, movementRadius));
+        var distance = newPos - originalPosition;
+        var time = distance.magnitude / movementSpeed;
+
+        iTween.MoveTo(gameObject, iTween.Hash("position", newPos, "easyType", movementEasyType, "time", time));
+        yield return new WaitForSeconds(time + 0.1f);
+        StartCoroutine(Movement());
+    }
+}
