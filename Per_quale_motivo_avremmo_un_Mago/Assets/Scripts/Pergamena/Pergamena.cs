52cbb7e2815203c6f0180d933bf34ae62760d570
Francioo87NABA <francesco_palma@studenti.naba.it>
2020-07-21 14:08:48 +0200

    Pergamene Illustrative
    Create e posizionate le pergamene illustrative

--- Per_quale_motivo_avremmo_un_Mago/Assets/Scripts/Pergamena/Pergamena.cs
+++ Per_quale_motivo_avremmo_un_Mago/Assets/Scripts/Pergamena/Pergamena.cs
@@ -1,18 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pergamena : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool play;
+    public bool newGame;
+    public bool exit;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Hand"))
+        {
+            if (play)
+            {
+                StartCoroutine(Play());
+            }
+            else if (newGame)
+            {
+                StartCoroutine(NewGame());
+            }
+            else if (exit)
+            {
+                StartCoroutine(Quit());
+            }      
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator NewGame()
     {
-        
+        //Instanzia l introduzione
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene(1);
     }
+
+    IEnumerator Play()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(1);
+    }
+
+    IEnumerator Quit()
+    {
+
+        yield return new WaitForSeconds(2f);
+        Application.Quit();
+    }
+
 }
