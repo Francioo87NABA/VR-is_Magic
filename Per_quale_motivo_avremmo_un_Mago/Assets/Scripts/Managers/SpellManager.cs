23052ec0a93f048cbb09ecacc7d10f1889da4ddb
Francioo87NABA <francesco_palma@studenti.naba.it>
2020-07-21 20:00:52 +0200

    Descrizione
    sistemati proiettili e tornado, messe a tutti gli instantiation prefabs gli audio source

--- Per_quale_motivo_avremmo_un_Mago/Assets/Scripts/Managers/SpellManager.cs
+++ Per_quale_motivo_avremmo_un_Mago/Assets/Scripts/Managers/SpellManager.cs
@@ -23,6 +23,7 @@ public class SpellManager : MonoBehaviour
     public Transform muroEtereoInstantiationTransform;
     public bool metamorfosi;
     public GameObject proiettile;
+    public GameObject muzzle;
     public Transform wandRay;
     public GameObject tornado;
     public Transform tornadoInstantiation;
@@ -92,17 +93,22 @@ public class SpellManager : MonoBehaviour
 
     IEnumerator Proiettili()
     {
-        for (int i = 0; i < 20; i++)
+        for (int i = 0; i < 10; i++)
         {
+            Instantiate(muzzle, wandRay.position, wandRay.rotation);
             Instantiate(proiettile, wandRay.position, wandRay.rotation);
-            yield return new WaitForSecondsRealtime(0.5f);
+            yield return new WaitForSecondsRealtime(0.3f);
         }
     }
 
     IEnumerator Tornado()
     {
-        Instantiate(tornado, tornadoInstantiation.position, tornadoInstantiation.rotation);
-        yield return new WaitForSecondsRealtime(5f);
-        Destroy(tornado);
+        tornado.SetActive(true);
+        yield return new WaitForSecondsRealtime(20f);
+        tornado.SetActive(false);
+
+        //Instantiate(tornado, tornadoInstantiation.position, tornadoInstantiation.rotation);
+        //yield return new WaitForSecondsRealtime(5f);
+        //Destroy(tornado);
     }
 }
